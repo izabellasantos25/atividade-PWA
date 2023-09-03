ebdfd913d7d2296141ffd13372761389517f66f5
iza <izabella.miranda@estudante.ifms.edu.br>
2023-09-02 20:21:58 -0400

    script funcinando

--- js/main.js
+++ js/main.js
@@ -7,3 +7,19 @@ window.onload = () => {
 
 const celulas = document.querySelectorAll(".celula");
 let jogar = true;
+
+const jogadorX = "X";
+const jogadorO = "O";
+
+document.addEventListener("click", (event) => {
+    if(event.target.matches(".celula")){
+        jogada(event.target.id);
+    }
+});
+
+function jogada(id){
+    const celula = document.getElementById(id);
+    jogo = jogar ? jogadorX : jogadorO;
+    celula.textContent = jogo;
+    jogar = !jogar;
+}
